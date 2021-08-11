1c3ab3e2e84f344f4e50358013e605a52ae9b74f
jujubebat <bbwwpark@naver.com>
2021-08-12 02:04:14 +0900

    feat: HttpServletResponse 기본 사용법 학습 완료

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.util.StreamUtils;
 
 @WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
@@ -19,11 +19,11 @@ public class RequestBodyJsonServlet extends HelloServlet {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
-        System.out.println("messageBody = "+ messageBody);
+        System.out.println("messageBody = " + messageBody);
 
         HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
 
--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.util.StreamUtils;
 
 @WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
 public class RequestBodyStringServlet extends HttpServlet {
 
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
